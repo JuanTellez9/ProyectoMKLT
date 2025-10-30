5e0d1ba60664f7c1eace7cc813a3f20955a5d384
MarianaMaya15 <mariana.rodriguezm@uqvirtual.edu.co>
2025-10-29 19:05:59 -0500

    se agrego nueva clase

---
+++ gym/src/main/java/uniquindio/edu/co/gym/model/Pago.java
@@ -0,0 +1,17 @@
+package edu.co.gym.model;
+
+import java.util.Date;
+
+public class Pago {
+    private String id;
+    private String detallesPago;
+    private double valor;
+    private Date  fechaPago;
+
+    public Pago(String id, String detallesPago, double valor, Date fechaPago) {
+        this.id = id;
+        this.detallesPago = detallesPago;
+        this.valor = valor;
+        this.fechaPago = fechaPago;
+    }
+}
