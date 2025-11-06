8ed5d2dc6f35fd04bb9af0cc0953db767f474e0d
Kebyn Julian Ochoa Perez <kebynj.ochoap@uqvirtual.edu.co>
2025-11-05 19:18:17 -0500

    java fx

--- gym/src/main/java/uniquindio/edu/co/gym/controller/PersonasController.java
+++ gym/src/main/java/uniquindio/edu/co/gym/controller/PersonasController.java
@@ -1,3 +1,21 @@
 package uniquindio.edu.co.gym.controller;
 
-public class PersonasController { /* sin lógica por ahora */ }
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PersonasController {
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtID;
+
+
+
+    @FXML
+    private void onGuardarPersona(){
+        String nombre = txtNombre.getText();
+
+        System.out.println(nombre);
+    }
+
+}
